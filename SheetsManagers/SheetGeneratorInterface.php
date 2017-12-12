bbfd4ed7908776fb74e52efc49a4522323ca3679
Alex Rock Ancelet <pierstoval@gmail.com>
2017-12-12 14:55:07 +0100

    Removed or renamed some classes

--- SheetsManagers/SheetGeneratorInterface.php
+++ SheetsManagers/SheetGeneratorInterface.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the PierstovalCharacterManagerBundle package.
  *
- * (c) Alexandre Rock Ancelet <alex.ancelet@gmail.com>
+ * (c) Alexandre Rock Ancelet <pierstoval@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,11 +13,13 @@ namespace Pierstoval\Bundle\CharacterManagerBundle\SheetsManagers;
 
 use Pierstoval\Bundle\CharacterManagerBundle\Model\CharacterInterface;
 
-interface SheetsManagerInterface
+interface SheetGeneratorInterface
 {
     /**
      * @param CharacterInterface $character
      * @param bool               $printer_friendly
+     *
+     * @return string|null Can return the name of the generated file if needed
      */
-    public function generateSheet(CharacterInterface $character, $printer_friendly = false);
+    public function generateSheet(CharacterInterface $character, bool $printer_friendly = false): ?string;
 }
