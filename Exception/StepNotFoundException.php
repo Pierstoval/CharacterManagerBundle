0f77afd00b649128abcd46047ddabf204017a834
Alex Rock Ancelet <pierstoval@gmail.com>
2017-12-12 14:57:05 +0100

    Add StepNotFoundException for specific cases

---
+++ Exception/StepNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the PierstovalCharacterManagerBundle package.
+ *
+ * (c) Alexandre Rock Ancelet <pierstoval@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pierstoval\Bundle\CharacterManagerBundle\Exception;
+
+class StepNotFoundException extends \RuntimeException
+{
+}
