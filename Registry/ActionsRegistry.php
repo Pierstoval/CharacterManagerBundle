10603a959e442a339207e3bda5bb5d9ef28dff4e
Alex Rock Ancelet <pierstoval@gmail.com>
2017-12-12 12:15:02 +0100

    Changed email and license header

--- Registry/ActionsRegistry.php
+++ Registry/ActionsRegistry.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the PierstovalCharacterManagerBundle package.
  *
- * (c) Alexandre Rock Ancelet <alex.ancelet@gmail.com>
+ * (c) Alexandre Rock Ancelet <pierstoval@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
