655b1e52461ca5dc09175c1db803a36e685a2ca1
Alex Rock Ancelet <pierstoval@gmail.com>
2017-12-26 20:15:52 +0100

    Fix missing StepResolver constructor arguments

--- DependencyInjection/PierstovalCharacterManagerExtension.php
+++ DependencyInjection/PierstovalCharacterManagerExtension.php
@@ -32,8 +32,8 @@ class PierstovalCharacterManagerExtension extends Extension
         $configuration = new Configuration();
         $config        = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         // Set all config in container.
         // Steps will be validated in compiler pass.
